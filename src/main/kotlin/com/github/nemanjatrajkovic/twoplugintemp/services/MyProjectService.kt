7b7f36cc551c6ba5818e5b23ca33d67972fee288
GitHub Action <action@github.com>
2023-11-13 21:53:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/nemanjatrajkovic/twoplugintemp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nemanjatrajkovic/twoplugintemp/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.nemanjatrajkovic.twoplugintemp.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nemanjatrajkovic.twoplugintemp.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
